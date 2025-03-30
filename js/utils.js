698d52befa77f31ff769d542d4a4e3eb325167a4
Anuj Sharma <anuj.shar2k8@gmail.com>
2025-03-30 11:19:52 +0530

    first commit

---
+++ js/utils.js
@@ -0,0 +1,9 @@
+const preloadImages = (selector = 'img') => {
+  return new Promise((resolve) => {
+      imagesLoaded(document.querySelectorAll(selector), {background: true}, resolve);
+  });
+};
+
+export {
+  preloadImages
+};
